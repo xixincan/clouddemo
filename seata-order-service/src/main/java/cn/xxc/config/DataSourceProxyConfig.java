f01f03f4c878b29344eebb3385cf642d255345c1
xixincan <xixincan@163.com>
2021-02-13 20:40:51 +0800

    Nacos + Seata整合

--- seata-order-service/src/main/java/cn/xxc/config/DataSourceProxyConfig.java
+++ seata-order-service/src/main/java/cn/xxc/config/DataSourceProxyConfig.java
@@ -1,7 +1,6 @@
 package cn.xxc.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import io.seata.rm.datasource.DataSourceProxy;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
@@ -14,6 +13,8 @@ import javax.sql.DataSource;
 /**
  * 不使用Spring Boot的自动装载数据源，主启动类中需要排除
  * 这里使用自己的数据源代理是为了是为了使用SEATA
+ * 其实这里也不需要自己数据源代理了，yml中配置了自动代理，所以DataSourceProxy注释掉了
+ * 这里自己到处是为了避免mybatis报错：缺少SqlSessionFactory或者SqlSessionTemplate
  *
  * created by xixincan
  * 2021/2/13
@@ -27,6 +28,7 @@ public class DataSourceProxyConfig {
         return new DruidDataSource();
     }
 
+//    配置了seata的自动代理，这里不需要手动代理了
 //    @Bean
 //    public DataSourceProxy dataSourceProxy(DataSource dataSource) {
 //        return new DataSourceProxy(dataSource);
